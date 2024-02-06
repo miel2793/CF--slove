1226defd726b2ebcfeb9d0528451f057bf3a9917
miel2793 <mahmudsifat2793@gmail.com>
2024-02-06 20:45:33 +0600

    Difference

--- simple calculator.cpp
+++ simple calculator.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
-    int X,Y;
-    cin>>X>>Y;
-   cout<<X<<" + "<<Y<<" = "<<X+Y<<endl;
-   cout<<X<<" * "<<Y<<" = "<<X*Y<<endl;
-   cout<<X<<" - "<<Y<<" = "<<X-Y<<endl;
-   
+int main() {
+
+    int X, Y;
+
+
+    cin >> X;
+
+    cin >> Y;
+
+
+    cout << X << " + " << Y << " = " << X + Y << endl;
+
+
+    cout << X << " * " << Y << " = " << X * Y << endl;
+
+    cout << X << " - " << Y << " = " << X - Y << endl;
+
+    return 0;
 }
