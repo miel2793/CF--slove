053d56a4bd82ed2604ab5dae2490be6d206518cf
miel2793 <mahmudsifat2793@gmail.com>
2024-02-06 21:01:29 +0600

    1 to n sum.cpp

---
+++ 1 to N sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N,sum=0;
+    cin>>N;
+    for(int i=1;i<=N;i++)
+    {
+      sum=sum+ i;
+    }
+    cout<<sum <<endl;
+    return 0;
+}
