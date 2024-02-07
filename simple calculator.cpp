1ee4e38a74c523d4451fce6badd307dbe0e98e07
miel2793 <mahmudsifat2793@gmail.com>
2024-02-07 07:48:15 +0600

    min max

--- simple calculator.cpp
+++ simple calculator.cpp
@@ -1,29 +1,13 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int A, B, C;
 
-    cin >> A >> B >> C;
-
-
-    int min = A;
-    if (B < min) {
-        min = B;
-    }
-    if (C < min) {
-        min = C;
-    }
-
-
-    int max = A;
-    if (B > max) {
-        max = B;
-    }
-    if (C > max) {
-        max = C;
-    }
-
-    cout<< min<<" " << max<<endl;
+int main()
+{
+    long long  X,Y;
+    cin>>X>>Y;
+    cout<<X<<" + "<<Y<<" = "<<X+Y<<endl;
+    cout<<X<<" * "<<Y<<" = "<<X*Y<<endl;
+    cout<<X<<" - "<<Y<<" = "<<X-Y<<endl;
 
     return 0;
 }
