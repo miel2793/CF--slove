cea0ead88266499c8a55cd94542ddb40a91b550e
miel2793 <mahmudsifat2793@gmail.com>
2024-05-21 16:33:54 +0600

    .

--- A. Bit++.cpp
+++ A. Bit++.cpp
@@ -1,23 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
 
+int main() {
     int n;
-    cin>>n;
-    int x=0;
-    for(int i=0;i<n;i++)
-    {
-
-        char o1,o2,ox;
-        cin>>o1>>ox>>o2;
-        if(o1=='+'||o2=='=')
-        {
+    cin >> n;
+    
+    int x = 0;
+    for (int i = 0; i < n; i++) {
+        char op[4];
+        cin >> op;
+   if (op[0] == '+' || op[1] == '+' || op[2] == '+') {
             x++;
+        } else {
+            x--; 
         }
-        else{
-            x--;
-        }
-        cout<<x<<endl;
     }
+
+   
+    cout << x <<endl;
+
+    return 0;
 }
