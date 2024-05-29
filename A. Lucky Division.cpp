65ff903acd4e2ee1fd9a5421d28031e69dde3a04
miel2793 <mahmudsifat2793@gmail.com>
2024-05-29 15:40:39 +0600

    ..

--- A. Lucky Division.cpp
+++ A. Lucky Division.cpp
@@ -1,5 +1,21 @@
 #include<iostream>
 using namespace std;
+
+/*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+\
+\           Solved BY 
+\
+\         
+\                       MIEL Mahmud Sifat
+\
+\        BSC Engg in CSE 
+\                                      
+\
+\                 BANGLADESH UNIVERSITY OF BUSINESS AND TECHNOLOGY (BUBT )
+\
+
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+*/
 int main()
 {
 
@@ -15,7 +31,7 @@ if(n%4==0||n%7==0 ||n%44==0||n%47==0||n%74==0||n%77==0||n%444==0|| n%447==0||n%4
 else
 {
 
-    cout<<"YES"<<endl;
+    cout<<"NO"<<endl;
 }
 }
 
