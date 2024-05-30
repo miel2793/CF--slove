b9e89afed57cc92e4f7cfa26218b8a138faea098
miel2793 <mahmudsifat2793@gmail.com>
2024-05-30 23:48:33 +0600

    .cpp

--- A. Mishka and Game.cpp
+++ A. Mishka and Game.cpp
@@ -1,37 +1,39 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int main()
 {
+    int n, a = 0, b = 0;
 
-    int n,a=0,b=0;
-    for(int i=0;i<n;i++)
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
     {
+        int m, l;
+        cin >> m >> l;
 
-        int m,l;
-        cin>>m>>l;
-        if(m>l)
+        if (m > l)
         {
-
             a++;
         }
-        else if(l>m)
+        else if (l > m)
         {
             b++;
         }
-        if(a>b)
-            {
-
-            cout<<"Mishka"<<endl;
-        }
-        else if(b>a)
-        {
-
-            cout<<"Chris"<<endl;
-        }
-        else if(a==b)
-        {
-
-            cout<<"Friendship is magic!^^"<<endl;
-        }
     }
+
+    if (a > b)
+    {
+        cout << "Mishka" << endl;
+    }
+    else if (b > a)
+    {
+        cout << "Chris" << endl;
+    }
+    else
+    {
+        cout << "Friendship is magic!^^" << endl;
+    }
+
+    return 0;
 }
