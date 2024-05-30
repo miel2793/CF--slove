a8bdb941e28f8ecf249c69345ed488708fc6b3c9
miel2793 <mahmudsifat2793@gmail.com>
2024-05-30 23:27:34 +0600

    .

---
+++ A. Mishka and Game.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int n,a=0,b=0;
+    for(int i=0;i<n;i++)
+    {
+
+        int m,l;
+        cin>>m>>l;
+        if(m>l)
+        {
+
+            a++;
+        }
+        else if(l>m)
+        {
+            b++;
+        }
+        if(a>b)
+            {
+
+            cout<<"Mishka"<<endl;
+        }
+        else if(b>a)
+        {
+
+            cout<<"Chris"<<endl;
+        }
+        else if(a==b)
+        {
+
+            cout<<"Friendship is magic!^^"<<endl;
+        }
+    }
+}
