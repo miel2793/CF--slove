cff1b78ce6c12965ad889c20977fbe37879845f8
miel2793 <mahmudsifat2793@gmail.com>
2024-06-04 00:27:18 +0600

    update 'Y. The last 2 digits.cpp'

--- Y. The last 2 digits.cpp
+++ Y. The last 2 digits.cpp
@@ -2,9 +2,21 @@
 using namespace std;
 int  main()
 {
-    int a,b,c,d;
+    long long int  a,b,c,d;
     cin>>a>>b>>c>>d;
-    int j= a*b*c*d;
-    int t=j%100;
-    cout<<t<<endl;
+    a=a%100;
+    b=b%100;
+    c=c%100;
+    d=d%100;
+    long long int j= a*b*c*d;
+    long long int t=j%100;
+    if(t<10)
+    {
+        cout<<0<<t<<endl;
+
+    }
+    else{
+ cout<<t<<endl;
+    }
+   
 }
